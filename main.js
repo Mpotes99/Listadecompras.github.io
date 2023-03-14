efa0ce1c49dcf638752137c114f5c9ffeafa29d7
Mauricio Potes <113395515+Mpotes99@users.noreply.github.com>
2023-03-14 20:34:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 (function(){
-	// Variables
+	
 	var lista = document.getElementById("lista"),
 		tareaInput = document.getElementById("tareaInput"),
 		btnNuevaTarea = document.getElementById("btn-agregar");
 
-	// Funciones
+	
 	var agregarTarea = function(){
 		var tarea = tareaInput.value,
 			nuevaTarea = document.createElement("li"),
@@ -17,13 +17,13 @@
 			return false;
 		}
 
-		// Agregamos el contenido al enlace
+		
 		enlace.appendChild(contenido);
-		// Le establecemos un atributo href
+		
 		enlace.setAttribute("href", "#");
-		// Agrergamos el enlace (a) a la nueva tarea (li)
+		
 		nuevaTarea.appendChild(enlace);
-		// Agregamos la nueva tarea a la lista
+		
 		lista.appendChild(nuevaTarea);
 
 		tareaInput.value = "";
@@ -44,15 +44,15 @@
 		this.parentNode.removeChild(this);
 	};
 
-	// Eventos
+	
 
-	// Agregar Tarea
+	
 	btnNuevaTarea.addEventListener("click", agregarTarea);
 
-	// Comprobar Input
+	
 	tareaInput.addEventListener("click", comprobarInput);
 
-	// Borrando Elementos de la lista
+	
 	for (var i = 0; i <= lista.children.length -1; i++) {
 		lista.children[i].addEventListener("click", eleminarTarea);
 	}
